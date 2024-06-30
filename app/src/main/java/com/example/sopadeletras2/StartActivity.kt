70edee1d645be96f3dee8d8909ed39bfed9846ac
Andrew <asoback@gmail.com>
2024-06-29 21:44:32 -0500

    Added start screen

---
+++ app/src/main/java/com/example/sopadeletras2/StartActivity.kt
@@ -0,0 +1,20 @@
+package com.example.sopadeletras2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        val startGameButton: Button = findViewById(R.id.startGameButton)
+        startGameButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
